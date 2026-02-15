27046e9839c5d5fe1a8312569ff422fd371b32bd
infernoWolf99 <kyeiseth58@gmail.com>
2026-02-15 21:42:57 +0000

    fixed art section

--- src/components/art.tsx
+++ src/components/art.tsx
@@ -1,53 +1,49 @@
-import { useMediaQuery } from "react-responsive";
-import { featureLists, goodLists } from "../utils/constants";
-import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { useMediaQuery } from "react-responsive";
+import { useGSAP } from "@gsap/react";
+import { featureLists, goodLists } from "../utils/constants";
 
 const Art = () => {
-  const isMobile = useMediaQuery({ maxWidth: 762 });
+  const isMobile = useMediaQuery({ maxWidth: 767 });
 
   useGSAP(() => {
     const start = isMobile ? "top 20%" : "top top";
 
-    const maskedTimeline = gsap.timeline({
+    const maskTimeline = gsap.timeline({
       scrollTrigger: {
         trigger: "#art",
-        start: start,
+        start,
         end: "bottom center",
         scrub: 1.5,
         pin: true,
       },
     });
 
-    maskedTimeline
-      .to("will-fade", { opacity: 0, stagger: 0.2, ease: "power1.inOut" })
+    maskTimeline
+      .to(".will-fade", { opacity: 0, stagger: 0.2, ease: "power1.inOut" })
       .to(".masked-img", {
         scale: 1.3,
         maskPosition: "center",
         maskSize: "400%",
         duration: 1,
-        ease: "power1.inOut",
+        ease: "power1.inOut ",
       })
-      .to("#masked-content", {
-        opacity: 1,
-        duration: 1,
-        ease: "power1.inOut",
-      });
+      .to("#masked-content", { opacity: 1, duration: 1, ease: "power1.inOut" });
   });
+
   return (
     <div id="art">
       <div className="container mx-auto h-full pt-20">
-        <h2 className="will-fade">The Art</h2>
+        <h2 className="will-fade">The ART</h2>
+
         <div className="content">
-          <ul className="will-fade space-y-4">
-            {goodLists.map((feature, index) => {
-              return (
-                <li key={index} className="flex items-center gap-2">
-                  <img src="/images/check.png" alt="check" />
-                  <p>{feature}</p>
-                </li>
-              );
-            })}
+          <ul className="space-y-4 will-fade">
+            {goodLists.map((feature, index) => (
+              <li key={index} className="flex items-center gap-2">
+                <img src="/images/check.png" alt="check" />
+                <p>{feature}</p>
+              </li>
+            ))}
           </ul>
 
           <div className="cocktail-img">
@@ -58,18 +54,13 @@ const Art = () => {
             />
           </div>
 
-          <ul className="will-fade space-y-4">
-            {featureLists.map((feature, index) => {
-              return (
-                <li
-                  key={index}
-                  className="flex items-center justify-start not-only:gap-2"
-                >
-                  <img src="/images/check.png" alt="check" />
-                  <p className="md:w-fit w-60">{feature}</p>
-                </li>
-              );
-            })}
+          <ul className="space-y-4 will-fade">
+            {featureLists.map((feature, index) => (
+              <li key={index} className="flex items-center justify-start gap-2">
+                <img src="/images/check.png" alt="check" />
+                <p className="md:w-fit w-60">{feature}</p>
+              </li>
+            ))}
           </ul>
         </div>
 
@@ -78,8 +69,8 @@ const Art = () => {
           <div id="masked-content">
             <h3>Made with Craft, Poured with Passion</h3>
             <p>
-              This isn't just a drink. It's a carefully crafted moment made just
-              for you
+              This isn’t just a drink. It’s a carefully crafted moment made just
+              for you.
             </p>
           </div>
         </div>
@@ -87,5 +78,4 @@ const Art = () => {
     </div>
   );
 };
-
 export default Art;
